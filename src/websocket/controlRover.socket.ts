95b0bf96e7e569cb110f6dfae0c2f789eea05788
Morgan Millet <millet.morgan5@gmail.com>
2023-07-08 10:28:10 +0200

    feat(package): update dependencies

--- src/websocket/controlRover.socket.ts
+++ src/websocket/controlRover.socket.ts
@@ -1,14 +1,11 @@
 import { Socket } from "socket.io";
 import MySocketInterface from "./mySocketInterface";
-import { Rover } from "../rover/rover";
 import { MissionControlInterface } from "../missionControl/missionControl.interface";
 
 class ControlRoverSocket implements MySocketInterface {
-  private readonly _rover: Rover;
   private readonly _missionControl: MissionControlInterface;
 
-  public constructor(rover: Rover, missionControl: MissionControlInterface) {
-    this._rover = rover;
+  public constructor(missionControl: MissionControlInterface) {
     this._missionControl = missionControl;
   }
 
